f5197745d73e8db191904c3357c4230b38b88eaa
Kihoon Lee <65799897+kihooncs@users.noreply.github.com>
2026-04-16 19:05:31 -0700

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // [2, 20, 4, 10, 3, 4, 5]
+        // [0, 3, 2, 5, 4, 6, 1, 1]
+
+        // store all in hash set
+        // for each element, if element is start of sequence, evaluate sequence 
+        // if not, move onto next item
+        // time complexity:
+        // best case: all elments are NOT sequence of each other -> O(n) time and O(n) space for set
+        // worst case: all elements are sequence of each other [1, 2, 3, 4, 5...] -> 
+        // (n - 1) + 1 + 1 + 1 + 1 + 1 + 1 + 1 = n
+        // each elment is visited 2 times at most 
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int longest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+            if (set.contains(curr - 1)) {
+                // not a start of a sequence
+                continue;
+            } else {
+                // start of a sequence
+                int seqLength = 1;
+                curr++;
+                while (set.contains(curr)) {
+                    seqLength++;
+                    curr++;
+                }
+                longest = Math.max(longest, seqLength);
+            }
+        }
+        return longest;
+    }
+}
