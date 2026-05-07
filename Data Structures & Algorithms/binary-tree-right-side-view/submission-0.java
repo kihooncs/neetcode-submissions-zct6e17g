bb0288e9b25ef56f5e00464ae45609f6e7ac0b30
Kihoon Lee <65799897+kihooncs@users.noreply.github.com>
2026-05-06 22:18:19 -0700

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode root, List<Integer> res, int depth) {
+        if (root == null) {
+            return;
+        } else {
+            if (res.size() == depth) {
+                res.add(root.val);
+            } else {
+                res.set(depth, root.val);
+            }
+            dfs(root.left, res, depth + 1);
+            dfs(root.right, res, depth + 1);
+        }
+    }
+}
